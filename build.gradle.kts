3aabf11e3a5596f70d2dfdc6fb58a47b88cae393
Thomas Platschorre <thomasplatschorre@gmail.com>
2020-09-26 09:25:12 +0200

    Recode

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.4.10"
+}
+
+group = "com.thoo.api"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
---
+++ src/main/kotlin/com/thoo/api/FortniteAPI.kt
@@ -0,0 +1,4 @@
+package com.thoo.api
+
+class FortniteAPI {
+}
